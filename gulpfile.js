9ddfcbb866d1b613a01ce49242833ce9682a8371
zhangdongming1989 <806684620@qq.com>
2016-08-26 15:12:14 +0800

    css minify

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
  */
 'use strict'
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    cssMin=require('gulp-minify-css'),
+    rename=require('gulp-rename');
 
 gulp.task('bootstrap', function () {
     gulp.src('bower_components/bootstrap/less/bootstrap.less')
@@ -13,4 +15,10 @@ gulp.task('bootstrap', function () {
 gulp.task('default', function () {
     gulp.watch('bower_components/bootstrap/less/*.less', ['bootstrap']);
     gulp.watch('bower_components/bootstrap/less/mixins/*.less', ['bootstrap']);
+});
+gulp.task('minifyCss',function () {
+    gulp.src('src/bootstrap.css')
+        .pipe(cssMin())
+        .pipe(rename('ani_bootstrap.min.css'))
+        .pipe(gulp.dest('src'))
 });
